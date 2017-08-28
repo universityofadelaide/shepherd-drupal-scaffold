bb75544ea13a1da281a956c6b6fd90ed9563dc76
caseyfw <casey.fulton@adelaide.edu.au>
2017-08-28 14:05:25 +0930

    Fixed RoboFile.php to include default install profile.

--- RoboFile.php
+++ RoboFile.php
@@ -14,14 +14,24 @@ include_once 'RoboFileBase.php';
 /**
  * Class RoboFile.
  */
-class RoboFile extends RoboFileBase {
+class RoboFile extends RoboFileBase
+{
 
-  /**
-   * {@inheritdoc}
-   */
-  public function __construct() {
-    parent::__construct();
-    // Put project specific overrides here, below the parent constructor.
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        // Put project specific overrides here, below the parent constructor.
+    }
 
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDrupalProfile()
+    {
+        // Replace this with the profile of your choice.
+        return "standard";
+    }
 }

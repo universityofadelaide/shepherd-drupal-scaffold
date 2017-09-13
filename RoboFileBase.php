b3e48a92fbe7893a7e6b95279f78928204de4517
Dave Mikolaj <dave@adelaide.edu.au>
2017-09-13 10:30:29 +0930

    Make the regex a little more specific, move all to one sed instruction.

--- RoboFileBase.php
+++ RoboFileBase.php
@@ -329,8 +329,7 @@ abstract class RoboFileBase extends \Robo\Tasks {
   protected function configExport($destination = NULL) {
     if ($destination) {
       $this->_exec("$this->drush_cmd -y cex --destination=" . $destination);
-      $this->_exec("sed -i '/^uuid: .*$/d' $this->application_root/$destination/*.yml");
-      $this->_exec("sed -i '/core:/, /default_config_hash:*/d' app/config_new/*.yml $this->application_root/$destination/*.yml");
+      $this->_exec("sed -i '/^uuid:.*$/d; /^_core:$/, /^.*default_config_hash:.*$/d' app/config_new/*.yml $this->application_root/$destination/*.yml");
     }
   }
 

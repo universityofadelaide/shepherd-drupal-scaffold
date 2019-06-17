680b7ef0050c0f74351bc70526f83a75d7bdd63b
Simon Lindsay <simon@iseek.biz>
2019-06-17 11:14:43 +0930

    Add sample config to docker-compose files.

--- src/Handler.php
+++ src/Handler.php
@@ -177,7 +177,7 @@ class Handler
             "  \$settings['cache_prefix']['default'] = getenv('REDIS_PREFIX') ?: '';\n" .
             "  // If we're not installing, include the redis services.\n" .
             "  if (!isset(\$GLOBALS['install_state'])) {\n" .
-            "    \$settings['cache']['default'] = 'cache.backend.redis';\n\n" .
+            "    \$settings['cache']['default'] = 'cache.backend.redis';\n" .
             "    \$settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';\n" .
             "  }\n" .
             "  if (getenv('REDIS_PASSWORD_FILE') || getenv('REDIS_PASSWORD')) {\n" .

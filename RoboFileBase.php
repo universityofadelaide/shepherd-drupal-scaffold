602ebf8b93fa564eb2b21f4f31e6213b8c8581d3
Simon Lindsay <simon.lindsay@adelaide.edu.au>
2019-12-02 16:38:18 +1030

    Support changed drush password syntax. (#77)

--- RoboFileBase.php
+++ RoboFileBase.php
@@ -595,7 +595,7 @@ abstract class RoboFileBase extends \Robo\Tasks {
     $this->_exec("$this->drush_cmd -y sql-drop");
     $this->_exec("$this->drush_cmd sqlq --file=$sql_file");
     $this->_exec("$this->drush_cmd cr");
-    $this->_exec("$this->drush_cmd upwd admin --password=password");
+    $this->_exec("$this->drush_cmd upwd admin password");
     $this->_exec("$this->drush_cmd updb --entity-updates -y");
     $this->say('Duration: ' . date_diff(new DateTime(), $start)->format('%im %Ss'));
     $this->say('Database imported, admin user password is : password');
